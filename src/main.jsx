52c43ce5dff2bfc538f11f05bda2c2412c9ae925
colnat <coltonbertrand14@gmail.com>
2024-12-25 14:44:18 -0400

    Added route for dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/register" element={<RegisterUser/>}/>
         <Route path="/login" element={<Login/>}/>
-        
+        <Route path="/" element={<Dashboard/>}/>
       </Routes>
     
   </BrowserRouter>
