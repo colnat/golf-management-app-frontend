0e60621bd3f8012b5370e64ee58a6e2ca1838a64
colton <coltonbertrand14@gmail.com>
2024-12-28 10:47:58 -0400

    User can add a 9 hole course

--- src/assets/Components/Dashboard.jsx
+++ src/assets/Components/Dashboard.jsx
@@ -8,6 +8,7 @@ const Dashboard = () => {
     const[course,setCourse] = useState({
         courseName:'',
         courseRating: 0,
+        courseLocation: '',
         courseHolesList: Array(9).fill({courseHoleNumber:0, courseHolePar: 0})
    
     });
@@ -52,8 +53,18 @@ const Dashboard = () => {
                                     name="courseRating"
                                     value={course.courseRating}
                                     onChange={handleChange}/>
+
+                            <label>Course Location</label>
+                            <input  className='input location'
+                                    type="text"
+                                    placeholder='Optional'
+                                    name="courseLocation"
+                                    value={course.courseLocation}
+                                    onChange={handleChange}/>
+                            
                         </div>
-                        <div>
+                        <div className='holes'>
+                            <div className='hole'>
                             
                             <h3>Hole 1</h3>
                             <label>Enter Hole Number</label>
@@ -68,9 +79,9 @@ const Dashboard = () => {
                                     name = "courseHolesList[0].courseHolePar"
                                     value = {course.courseHolesList[0].courseHolePar}
                                     onChange={handleChange}/>
-                       </div>   
+                            </div>
 
-                       <div>
+                            <div className='hole'>
                             
                             <h3>Hole 2</h3>
                             <label>Enter Hole Number</label>
@@ -85,7 +96,130 @@ const Dashboard = () => {
                                     name = "courseHolesList[1].courseHolePar"
                                     value = {course.courseHolesList[1].courseHolePar}
                                     onChange={handleChange}/>
-                       </div>  
+                            </div>
+                            
+                            <div className='hole'>
+                            
+                            <h3>Hole 3</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[2].courseHoleNumber"
+                                    value = {course.courseHolesList[2].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[2].courseHolePar"
+                                    value = {course.courseHolesList[2].courseHolePar}
+                                    onChange={handleChange}/>
+                            </div>
+
+                            <div className='hole'>
+                            
+                            <h3>Hole 4</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[3].courseHoleNumber"
+                                    value = {course.courseHolesList[3].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[3].courseHolePar"
+                                    value = {course.courseHolesList[3].courseHolePar}
+                                    onChange={handleChange}/>
+                            </div>
+                            <div className='hole'>
+                            
+                            <h3>Hole 5</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[4].courseHoleNumber"
+                                    value = {course.courseHolesList[4].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[4].courseHolePar"
+                                    value = {course.courseHolesList[4].courseHolePar}
+                                    onChange={handleChange}/>
+                            </div>
+
+                            <div className='hole'>
+                            
+                            <h3>Hole 6</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[5].courseHoleNumber"
+                                    value = {course.courseHolesList[5].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[5].courseHolePar"
+                                    value = {course.courseHolesList[5].courseHolePar}
+                                    onChange={handleChange}/>
+                            </div>
+                       </div>   
+                       
+                       <div className='holes'>
+                       
+                       <div className='hole'>
+                            
+                            <h3>Hole 7</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[6].courseHoleNumber"
+                                    value = {course.courseHolesList[6].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[6].courseHolePar"
+                                    value = {course.courseHolesList[6].courseHolePar}
+                                    onChange={handleChange}/>
+                        </div>
+
+                        <div className='hole'>
+                            
+                            <h3>Hole 8</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[7].courseHoleNumber"
+                                    value = {course.courseHolesList[7].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[7].courseHolePar"
+                                    value = {course.courseHolesList[7].courseHolePar}
+                                    onChange={handleChange}/>
+                        </div>
+
+                        <div className='hole'>
+                            
+                            <h3>Hole 9</h3>
+                            <label>Enter Hole Number</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[8].courseHoleNumber"
+                                    value = {course.courseHolesList[8].courseHoleNumber}
+                                    onChange={handleChange}/>
+                            <label>Enter the Par</label>
+                            <input  className='input'
+                                    type= "number"
+                                    name = "courseHolesList[8].courseHolePar"
+                                    value = {course.courseHolesList[8].courseHolePar}
+                                    onChange={handleChange}/>
+                        </div>
+                       
+                       </div> 
                        <button className="button" type="submit" onClick={addCourse}>Save Course </button>
                     </form>
                 </div>
